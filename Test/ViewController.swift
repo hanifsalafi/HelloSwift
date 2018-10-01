3fd3cd776f6ba8ca9f2680e5f662bb194d663272
Hanif Salafi <hanifsalafi@hanifs-macbook-pro.local>
2018-10-01 17:50:56 +0700

    Initial Commit

---
+++ Test/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Test
+//
+//  Created by Hanif Salafi on 01/10/18.
+//  Copyright © 2018 Hanif Salafi. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
